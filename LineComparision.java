e473eb3fcb072e951178dfa32527872a7693f379
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-06 23:35:24 +0530

    Refactor: call the getcortinates check equility of lines and compare two lines method from main method

--- LineComparision.java
+++ LineComparision.java
@@ -23,6 +23,9 @@ public class LineComparision {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to line comparison computation");
+		gettingCoordinates();
+        checkEqualityOfLine();
+        compareTwoLines();
 		
 	}
 	
