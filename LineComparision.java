ef2eeaa690664b6c5b4b22ba1ac9b9287897473f
Nomercy-ops <chauhanrikesh786@gmail.com>
2021-06-06 23:16:36 +0530

    Added: welcome to line comparision computation message

---
+++ LineComparision.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Rikesh Chhetri
+ * @Created_on: 06.06.21
+ * 
+ * purpose: Line comparison program to compare length of two lines with co-ordinates.
+ * To model a line co-ordinate Cartesian system is used and length is calculated.
+ * 
+ * 
+ */
+
+package com.bridgelabz.lineComparision;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to line comparison computation");
+		
+	}
+
+	
